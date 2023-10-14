78489a877a8b7ba58070f962b11deb6be015322c
Git_Learner <85446707+Git-Souma@users.noreply.github.com>
2023-10-15 01:01:37 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+void sum(int *a,int *b){
+    *a=a+b;
+}
+int main()
+{
+   int a=5;
+   int b=6;
+   sum(&a,&b);
+   
+    cout<<a<<""<<b;
+
+    return 0;
+}
